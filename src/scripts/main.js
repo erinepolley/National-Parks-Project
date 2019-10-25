6fe4ad435be376aade236b520255c23cf0a9d5e6
Erin Polley <erinepolley@gmail.com>
2019-10-25 15:53:11 -0500

    currently trying to add borders to each park name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,20 @@ console.log("You will get through this!")
 //display the park name and the state the park is located in in this HTML string. then...
 //take the HTML string and put it in this div tag on the DOM. 
 
+
+
+
 const parkResultsToHtml = (parkObj) => {
-   return `
+    let parkInHtml = `
 <article>
     <h3>${parkObj.name}</h3>
     <p>${parkObj.state}</p>
 </article>
-`}
+`
+    return parkInHtml;
+
+}
+
 
 let parkListToHtml = "";
 const parksDisplay = (parksResultsArray) => {
@@ -21,7 +28,11 @@ const parksDisplay = (parksResultsArray) => {
         console.log(park)
         parkListToHtml += parkResultsToHtml(park)
         divThatListWillGoIn.innerHTML = parkListToHtml
-        console.log(parkListToHtml)
+        addClassNameToElement(park);
+
+
+
+
     })
 
 }
@@ -29,12 +40,22 @@ const parksDisplay = (parksResultsArray) => {
 
 const fetchNationalParksData = () => {
     fetch("http://localhost:8088/parks")
-    .then(response => response.json())
-    .then(parsedParks => parksDisplay(parsedParks))
+        .then(response => response.json())
+        .then(parsedParks => parksDisplay(parsedParks))
 }
 
+const addClassNameToElement = (parkObj) => {
+    let articleOnDom = document.querySelector("article")
+    if (parkObj.visited) {
+        articleOnDom.classList.add("visited")
+    } else {
+        articleOnDom.classList.add("not-visited")
+    }
+}
 
 fetchNationalParksData();
+// addClassNameToElement();
+
 
 
         // const parkDisplay = (parkSearchResultsArray) => {
